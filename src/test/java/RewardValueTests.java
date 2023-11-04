8c827fb2ee249e2f10e94cb3017931548897ccdc
Ganesh Avula <ganesh.avula123@gmail.com>
2023-11-04 17:47:50 -0500

    revert changed to do pull request

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,17 +20,12 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        rewardValue.convertFromCashToMiles();
-        assertEquals(cashValue / 0.0035, rewardValue.getMilesValue(), 0.001);
+        assert false;
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        rewardValue.convertFromMilesToCash();
-        assertEquals(milesValue * 0.0035, rewardValue.getCashValue(), 0.001);
+        assert false;
     }
 }
+
